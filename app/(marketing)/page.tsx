85b688763b5862f2a1d9e8c02bc83f1217059884
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-19 21:39:27 +0530

    hero heading and footer created also imported all the require image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,15 @@
-import { Button } from "@/components/ui/button";
+import Footer from "./_components/footer";
+import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
+
 function MarketingPage() {
   return (
-    <div>
-      <Button>click me</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
     </div>
   );
 }
