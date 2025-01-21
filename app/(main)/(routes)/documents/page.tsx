fdbce2baa4611368e6e74ca957f2921059bf3155
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-21 16:18:36 +0530

    empty page created

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 function DocumentsPage() {
-  return <div>this is protecteed page</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-block.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:bock"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Zotion
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 }
 
 export default DocumentsPage;
