b217d42305d9a34bcde97b4e806b8747f5e4cd6d
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-27 10:57:07 +0530

    feat: implemented upload cover image and remove cover image

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,5 +1,64 @@
 "use client";
 
-export function Cover() {
-  return <div className="h-[50vh]">Cover image</div>;
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import { ImageIcon, X } from "lucide-react";
+
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import { useCoverImage } from "@/hooks/use-cover-image";
+
+type CoverImageProps = {
+  url?: string;
+  preview?: boolean;
+};
+
+export function Cover({ url, preview }: CoverImageProps) {
+  const coverImage = useCoverImage();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const params = useParams();
+
+  function onRemove() {
+    removeCoverImage({
+      id: params.documentId as Id<"documents">,
+    });
+  }
+  return (
+    <div
+      className={cn(
+        "relative w-full h-[35vh] group",
+        !url && "h-[12vh]",
+        url && "bg-muted"
+      )}
+    >
+      {!!url && <Image src={url} fill alt="Cover" className="object-cover" />}
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={coverImage.onOpen}
+            variant="outline"
+            size="sm"
+            className="text-muted-foreground text-xs"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            Change cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            variant="outline"
+            size="sm"
+            className="text-muted-foreground text-xs"
+          >
+            <X className="h-4 w-4 mr-2" />
+            Remove
+          </Button>
+        </div>
+      )}
+    </div>
+  );
 }
