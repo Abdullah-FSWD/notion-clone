5afddb6720d7727feb7cf98759af530f0ca343b7
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-28 17:57:34 +0530

    fix: spelling correction

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -11,7 +11,7 @@ function Heading() {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas💡, Documents📕 & Plans🚀. Welcome to{" "}
+        Your Ideas💡,Documents📕 & Plans🚀. Welcome to{" "}
         <span className="underline">Zotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
